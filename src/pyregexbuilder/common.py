981ecd83919f16198b685c8aac42ec101825c8ca
jsquaredosquared <jeffreyjamesochiengodongo@gmail.com>
2025-05-21 07:12:28 +0800

    feat: add support for inline flags

--- src/pyregexbuilder/common.py
+++ src/pyregexbuilder/common.py
@@ -1,9 +1,20 @@
-import regex as re
+from typing import Literal, TypedDict
 from abc import ABC
+import regex as re
 
 
 class RegexBuilderException(Exception):
     pass
+
+
+class RegexFlagsDict(TypedDict, total=False):
+    ASCII: Literal[True]
+    IGNORECASE: bool
+    LOCAL: Literal[True]
+    MULTILINE: bool
+    DOTALL: bool
+    UNICODE: Literal[True]
+    VERBOSE: bool
 
 
 class RegexComponent(ABC):
@@ -19,6 +30,34 @@ class RegexComponent(ABC):
             for component in components
         )
 
+    def with_flags(self, flags: RegexFlagsDict):
+        flags_shorthand = {
+            "ASCII": "a",
+            "IGNORECASE": "i",
+            "LOCAL": "L",
+            "MULTILINE": "m",
+            "DOTALL": "s",
+            "UNICODE": "u",
+            "VERBOSE": "x",
+        }
+
+        if sum((flag in flags for flag in ["ASCII", "LOCAL", "UNICODE"])) > 1:
+            raise RegexBuilderException(
+                "The flags ASCII, LOCAL, and UNICODE are mutually exclusive. "
+                "Specify only one of them."
+            )
+
+        flags_to_set = set(
+            flags_shorthand[flag] for flag in filter(lambda f: flags[f], flags)
+        )
+        flags_to_remove = set(
+            flags_shorthand[flag] for flag in filter(lambda f: not flags[f], flags)
+        )
+
+        self.regex = rf"(?{''.join(flags_to_set)}{"-"+''.join(flags_to_remove) if flags_to_remove else ''}:{self.regex})"
+
+        return self
+
 
 class RegexString(RegexComponent):
     def __init__(self, string: str) -> None:
@@ -29,8 +68,8 @@ class Regex(RegexComponent):
     def __init__(self, *components: str | RegexComponent) -> None:
         self.regex = self.parse(*components)
 
-    def compile(self, flags=0):
-        return re.compile(self.regex, flags)
+    def compile(self, *args, **kwargs):
+        return re.compile(self.regex, *args, **kwargs)
 
 
 class ChoiceOf(RegexComponent):
